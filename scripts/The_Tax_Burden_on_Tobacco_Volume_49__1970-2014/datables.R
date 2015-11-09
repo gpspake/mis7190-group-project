73494549477bfe9efe1bd13cd75a2779f17fe5d4
gpspake <gpspake@gmail.com>
2015-11-08 19:32:14 -0600

    add datatables script

---
+++ scripts/The_Tax_Burden_on_Tobacco_Volume_49__1970-2014/datables.R
@@ -0,0 +1,11 @@
+#The_Tax_Burden_on_Tobacco_Volume_49__1970-2014
+
+setwd( dirname(sys.frame(1)$ofile) )
+taxData <- read.csv(file="The_Tax_Burden_on_Tobacco_Volume_49__1970-2014.csv")
+
+#print( names(data) )
+library(DT)
+
+print( datatable( taxData ) )
+
+typeof(TRUE)
